c68dd1a7ba69bab343b0be5155239864c3d759ab
oscar31 <1dam.oinvernon@institutoserpis.org>
2015-12-10 10:27:54 +0100

    añadido GHolaMundo

---
+++ GHolaMundo/src/main/java/org/institutoserpis/ad/HolaMndo.java
@@ -0,0 +1,14 @@
+package org.institutoserpis.ad;
+
+public class HolaMndo {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hola Mundo desde eclipse con gradle");
+		
+		
+		
+		
+	}
+
+}
